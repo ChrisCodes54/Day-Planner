be5ef13d08a6627c7fd1170945dfdc151855708d
ChrisCodes54 <christangarife95@gmail.com>
2021-11-12 19:57:55 -0500

    created time blocks

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ var update = function() {
     $("#currentDay").text(currentdate);
 }
 setInterval(update, 1000);
+
+// first lets set up the boxes, we did this through HTML, but will try and make it here.
+var containerEl = $('<div>').addClass('container')
+var sectionrows = $('<section>').addClass('row time-block');
+var divhours = $('<div>').addClass('col-2 hour');
+var textarea = $('<textarea>').addClass('col-9 description');
+var savebutton = $('<button').addClass('col-1 saveBtn')
+sectionrows.append(divhours, textarea, savebutton);
+containerEl.append(sectionrows);
+
+
+
+//then lets set up the times on the left handside?
+
+/*then we can focus on having the middle box be where they can write their activities
+which means we will need to use local storage to achieve this */
